5c02dbecb8e4c9dd87d1d5553b16c225be06aa17
AminovS <seanster1616@gmail.com>
2023-10-22 23:58:25 -0700

    Update CollidableObject.cs

--- UnityTest/Assets/Scripts/Collidable/CollidableObject.cs
+++ UnityTest/Assets/Scripts/Collidable/CollidableObject.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CollidableObject : MonoBehaviour
 {
+    private Collider2D z.Collider;
+    private ContactFilter2D z_Filter;
+    private List<Collider2D> z_CollidedObjects;
     // Start is called before the first frame update
     void Start()
     {
-        
+        z_Collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
+        foreach(var o in z_CollidedObjects) {
+            Debug.Log("Collided with " + o.name);
+        }
     }
 }
