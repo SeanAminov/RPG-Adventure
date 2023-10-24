71963eb514df08eb0e72edc04fb5daec690d6b00
AminovS <seanster1616@gmail.com>
2023-10-23 22:30:37 -0700

    Fixed Up Chest and Made it Collidable
    Press e on the leftmost chest to get debug statement.

--- UnityTest/Assets/Scripts/Collidable/CollidableObject.cs
+++ UnityTest/Assets/Scripts/Collidable/CollidableObject.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class CollidableObject : MonoBehaviour
 {
     private Collider2D z_Collider;
+    [SerializeField]
     private ContactFilter2D z_Filter;
-    private List<Collider2D> z_CollidedObjects;
+    private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
     // Start is called before the first frame update
-    void Start() 
+    protected virtual void Start() 
     {
         z_Collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
         foreach(var o in z_CollidedObjects) {
-            Debug.Log("Collided with " + o.name);
+           OnCollided(o.gameObject);
         }
     }
-    */
+
+    protected virtual void OnCollided(GameObject collidedObject) {
+        Debug.Log("Collided with " + collidedObject.name);
+    }
+    
 }
